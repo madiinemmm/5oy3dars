135b1f3979239703155a18cb3d6b4436386345b9
madiinemm <uraimovamadina179@gmail.com>
2023-12-28 00:12:35 +0500

    uyga vazifa bajarildi

---
+++ index.js
@@ -0,0 +1,17 @@
+const block1 = document.querySelector('#block');
+const card1 = document.querySelector('#card');
+const btn1 = document.querySelector('#btn');
+const txt = document.getElementsByClassName('main__txt');
+
+
+
+btn1.addEventListener('click', function() {
+   img.setAttribute('src', 'https://picsum.photos/id/238/200/300');
+   card1.setAttribute('class', 'card', 'btn1', 'txt');
+});   
+
+
+
+
+   
+
